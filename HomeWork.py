02b1bc6ca33f41835f93cb1d9d1c46f04c4434c1
Javoxir Umarkulov <zavohirumarkulov23@gmail.com>
2023-11-28 16:59:51 +0500

    Character Input

---
+++ HomeWork.py
@@ -0,0 +1,8 @@
+
+#! 1. Character Input
+#*   Create a program that asks the user to enter their name and their age.
+
+your_name = input("Enter your name: ")
+your_age = input("Enter your age: ")
+print(f'Name: {your_name}')
+print(f'Age: {your_age}')
